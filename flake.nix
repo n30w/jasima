0276aecb17467c35c8abe542467e7484970b4cbc
Neo <99268950+n30w@users.noreply.github.com>
2025-02-28 20:07:19 +0800

    chore: up `flake.nix` and `direnv`

--- flake.nix
+++ flake.nix
@@ -69,9 +69,9 @@
             fish
           ];
           
-          shellHook = ''
-            exec fish
-          '';
+          # shellHook = ''
+          #   exec fish
+          # '';
         };
       });
 
