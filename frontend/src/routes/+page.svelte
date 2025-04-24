908a662d0d4935892fd5e27d4a5566bfd7776d11
Neo <99268950+n30w@users.noreply.github.com>
2025-04-24 22:01:23 +0800

    feat: add `SvelteMarkdown`

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
 	import { EventSource } from 'eventsource';
 	import { tick } from 'svelte';
+	import SvelteMarkdown from '@humanspeak/svelte-markdown';
 
-	// const eventSrc = new EventSource('http://127.0.0.1:7070/time');
 	const src = 'http://127.0.0.1:7070/events';
-	const src2 = 'http://127.0.0.1:7070/time';
 
 	let messages: { message; timestamp: string }[] = $state.raw([]);
 	let element: HTMLElement;
 
 	$effect.pre(() => {
-		const autoscroll =
-			messages && element && element.offsetHeight + element.scrollTop > element.scrollHeight - 50;
+		const autoscroll = messages && element;
 		if (autoscroll) {
 			tick().then(() => {
 				element.scroll({ top: element.scrollHeight, behavior: 'smooth' });
@@ -23,16 +21,12 @@
 		const es = new EventSource(src);
 
 		es.onmessage = (event) => {
-			// console.log(messages);
 			try {
 				const json = JSON.parse(event.data);
-				// console.log(json);
 				messages = [...messages, json];
 			} catch (err) {
 				console.error('Failed to parse JSON from event:', err);
 			}
-			// messages = [...messages, event.data];
-			// v = event.data;
 		};
 
 		es.onerror = (err) => {
@@ -46,10 +40,15 @@
 
 <!-- <div class="h-1/2"> -->
 <ul class="h-screen overflow-auto" bind:this={element}>
-	{#each messages as { message, timestamp, text, sender, command }, i (i)}
-		<!-- {#if command == 0} -->
-		<li class="m-2 border-b-1 p-2 text-lg"><strong>{timestamp} {sender}</strong>: {text}</li>
-		<!-- {/if} -->
+	{#each messages as { timestamp, text, sender, command }, i (i)}
+		{#if command == 0}
+			<li class="m-2 border-b-1 p-2 font-mono text-base">
+				<strong>{timestamp} {sender}</strong>:
+				<article>
+					<SvelteMarkdown source={text} />
+				</article>
+			</li>
+		{/if}
 	{/each}
 </ul>
 <!-- </div> -->
