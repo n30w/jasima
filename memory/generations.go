c609cb6c47a64e417ee09689b526fac2adee4c68
Neo <99268950+n30w@users.noreply.github.com>
2025-04-30 00:41:24 +0800

    up generation types

--- memory/generations.go
+++ memory/generations.go
@@ -1,18 +1,79 @@
 package memory
 
-import "codeberg.org/n30w/jasima/chat"
+import (
+	"encoding/json"
+	"maps"
+
+	"codeberg.org/n30w/jasima/chat"
+)
+
+type TranscriptGeneration map[chat.Layer][]Message
+
+func (t TranscriptGeneration) Copy() TranscriptGeneration {
+	newMap := make(TranscriptGeneration)
+
+	for k, v := range t {
+		m := make([]Message, len(v))
+		copy(m, v)
+		newMap[k] = m
+	}
+
+	return newMap
+}
 
 type LogographyGeneration map[string]string
 
+func (l LogographyGeneration) Copy() LogographyGeneration {
+	newMap := make(LogographyGeneration)
+	maps.Copy(newMap, l)
+	return newMap
+}
+
 type SpecificationGeneration map[chat.Layer]chat.Content
 
-type DictionaryGeneration map[string]string
+func (s SpecificationGeneration) Copy() SpecificationGeneration {
+	newMap := make(SpecificationGeneration)
+	maps.Copy(newMap, s)
+	return newMap
+}
+
+type DictionaryGeneration map[string]DictionaryEntry
+
+func (d DictionaryGeneration) Copy() DictionaryGeneration {
+	newMap := make(DictionaryGeneration)
+	maps.Copy(newMap, d)
+	return newMap
+}
+
+func (d DictionaryGeneration) String() string {
+	// There is probably a better way to do this.
+	dictArr := make([]DictionaryEntry, len(d))
+
+	for _, entry := range d {
+		dictArr = append(dictArr, entry)
+	}
+
+	s, _ := json.Marshal(dictArr)
+
+	return string(s)
+}
+
+type DictionaryEntry struct {
+	Word       string `json:"word"`
+	Definition string `json:"definition"`
+
+	// Remove represents whether a word should be removed from the dictionary.
+	// This is used when sending data to and from an agent. If an agent is
+	// queried to remove an entry from the dictionary, this field would be
+	// set to `true`.
+	Remove bool `json:"remove"`
+}
 
 // Generation contains all generational information related to a single
 // iteration of a conlang's development.
 type Generation struct {
-	Transcript     []Message
-	Logography     LogographyGeneration
-	Specifications SpecificationGeneration
-	Dictionary     DictionaryGeneration
+	Transcript     TranscriptGeneration    `json:"transcript"`
+	Logography     LogographyGeneration    `json:"logography"`
+	Specifications SpecificationGeneration `json:"specifications"`
+	Dictionary     DictionaryGeneration    `json:"dictionary"`
 }
