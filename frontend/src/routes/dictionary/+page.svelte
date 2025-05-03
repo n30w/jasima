fbcc3acbfa57aa86ec6645038ec01a4f13c797fb
Neo <99268950+n30w@users.noreply.github.com>
2025-05-03 16:31:45 +0800

    impl dict page

--- frontend/src/routes/dictionary/+page.svelte
+++ frontend/src/routes/dictionary/+page.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
+	import { typewriter } from '$lib/typewriter';
+	import { fade } from 'svelte/transition';
+
 	const src = 'http://127.0.0.1:7070/test/generations';
 
-	let currentDict: dictionaryEntry[] = $state([]);
+	let currDict: Map<string, DictionaryEntry> = $state(new Map<string, DictionaryEntry>());
 
 	$effect(() => {
 		const es = new EventSource(src);
 		es.onmessage = (event) => {
 			try {
-				const json: generation = JSON.parse(event.data);
-				const nd = [];
+				const json: Generation = JSON.parse(event.data);
 				for (const [key, value] of Object.entries(json.dictionary)) {
-					nd.push(value);
+					currDict.set(key, value);
 				}
-				currentDict = nd;
-				console.log(json);
+				currDict = new Map(currDict);
 			} catch (err) {
 				console.log('Failed to parse JSON from /generations', err);
 			}
@@ -28,11 +29,23 @@
 	});
 </script>
 
-<div class="grid grid-cols-16 gap-1">
-	{#each currentDict as d, i (i)}
-		<div class="border-1 p-2 hover:bg-neutral-100">
-			<h1 class="text-lg font-bold">{d.word}</h1>
-			<p class="text-xs">{d.definition}</p>
-		</div>
+<div class="flex flex-wrap gap-1">
+	{#each currDict as [key, value] (key)}
+		{#if key}
+			<div class="h-fit max-w-1/3 border-1 p-2 hover:bg-neutral-100">
+				<h1 class="text-base font-bold">
+					{#key value.word}
+						<!-- fade the word whenever it changes -->
+						<span in:fade>{value.word}</span>
+					{/key}
+				</h1>
+				{#key value.definition}
+					<!-- fade the definition whenever it changes -->
+					<p class="text-xs" in:typewriter={{ speed: 10 }}>
+						{value.definition}
+					</p>
+				{/key}
+			</div>
+		{/if}
 	{/each}
 </div>
