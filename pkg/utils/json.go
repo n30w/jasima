4496ec295dae211b563328dcb72909f31a9cf00b
Neo <99268950+n30w@users.noreply.github.com>
2025-05-10 02:07:01 +0800

    feat: json utils

--- pkg/utils/json.go
+++ pkg/utils/json.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 
 	"github.com/invopop/jsonschema"
+	"github.com/pkg/errors"
 )
 
 // GenerateSchema uses jsonschema library to make a JSON schema.
@@ -17,6 +18,23 @@ func GenerateSchema[T any]() any {
 	var v T
 	schema := reflector.Reflect(v)
 	return schema
+}
+
+func GenerateJsonSchema[T any]() ([]byte, error) {
+	reflector := jsonschema.Reflector{
+		AllowAdditionalProperties: false,
+		DoNotReference:            true,
+	}
+	var v T
+	schema := reflector.Reflect(v)
+	schema.ID = ""
+	schema.Version = ""
+	schema.AdditionalProperties = nil
+	sc, err := json.Marshal(schema)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal JSON")
+	}
+	return sc, nil
 }
 
 func Unmarshal[T any](s string) (
@@ -33,4 +51,20 @@ func Unmarshal[T any](s string) (
 	}
 
 	return d, nil
+}
+
+func StructToMap(s any) (map[string]any, error) {
+	var m map[string]any
+
+	b, err := json.Marshal(s)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
 }
