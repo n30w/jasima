56d7a42749c3e51213c0379846c22433fd0a1f28
Neo <99268950+n30w@users.noreply.github.com>
2025-05-18 05:20:48 +0800

    feat: timer truncate

--- pkg/utils/timing.go
+++ pkg/utils/timing.go
@@ -4,8 +4,11 @@ import (
 	"time"
 )
 
+// Timer makes a timer that starts at a specified time. To stop the timer,
+// call the function that is returned. The duration is truncated to the
+// thousandth's place of a second.
 func Timer(start time.Time) func() time.Duration {
 	return func() time.Duration {
-		return time.Since(start)
+		return time.Since(start).Truncate(time.Millisecond)
 	}
 }
